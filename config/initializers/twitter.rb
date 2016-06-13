078a46d7204da993c5a2080799cfd69686aa16e4
KT Khoo <khoodose@gmail.com>
2016-06-13 23:21:08 +0800

    take out twitter keys

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,7 +1,7 @@
 
 $client = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'KcwMHLwxTXReF1t4TvYsUG4Pg'
-  config.consumer_secret = 'M5u3tbzL6sOF90aE8LGD9BqF5Xq2G8fOwwTJzarRVtg4BOT4Bd'
-  config.access_token = '742317448787955712-KilKHzq9ynMw7JAwOeqVYoRorHZgz5H'
-  config.access_token_secret = 'J0zvf0befWMvNfqRLS96whVSdQlHIrotvnDevV8RNQO9e'
+  config.consumer_key = "YOUR_CONSUMER_KEY"
+  config.consumer_secret = "YOUR_CONSUMER_SECRET"
+  config.access_token = "YOUR_ACCESS_TOKEN"
+  config.access_token_secret = "YOUR_ACCESS_SECRET"
 end
