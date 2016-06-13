f9235a9dcc794fddcc6799febec675f9e44ad37d
KT Khoo <khoodose@gmail.com>
2016-06-13 23:12:49 +0800

    edit readme

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -2,6 +2,6 @@
 $client = Twitter::REST::Client.new do |config|
   config.consumer_key = 'KcwMHLwxTXReF1t4TvYsUG4Pg'
   config.consumer_secret = 'M5u3tbzL6sOF90aE8LGD9BqF5Xq2G8fOwwTJzarRVtg4BOT4Bd'
-  config.access_token = '742317448787955712-1pguG1ofA3y69gnSshmSPSSMb1vEnO7'
-  config.access_token_secret = 'G7nRT9Jo0kHKj07aRqoZYaKyZgTzO99WzPjD3d6Ggzajh'
+  config.access_token = '742317448787955712-KilKHzq9ynMw7JAwOeqVYoRorHZgz5H'
+  config.access_token_secret = 'J0zvf0befWMvNfqRLS96whVSdQlHIrotvnDevV8RNQO9e'
 end
